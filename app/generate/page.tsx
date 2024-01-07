42679f4c141b9e4a320be1d793a77b968a8a1c07
Prudhvi Suraaj <86603322+GPSxtreme@users.noreply.github.com>
2024-01-07 18:05:14 +0530

    Update page.tsx

--- app/generate/page.tsx
+++ app/generate/page.tsx
@@ -1,70 +1,211 @@
 "use client";
 import { useAction } from "next-safe-action/hooks";
-import { FormEvent, useEffect } from "react";
+import { FormEvent } from "react";
+import { GenerateMetatagsInput, generateMetatags } from "./actions";
 import {
-  GenerateMetatagsInput,
-  RobotInput,
-  SettingsInput,
-  generateMetatags,
-} from "./actions";
+  pageMetaData,
+  projectMetaData,
+  robotCheckboxes,
+  settingsCheckboxes,
+} from "../lib/data";
+import { useGeneratedOutput } from "../state/generatedOutput";
+import { useRouter } from "next/navigation";
 
 export default function MetaForm() {
   const { execute, status, result } = useAction(generateMetatags);
+  const setOutput = useGeneratedOutput((state) => state.setOutput);
+  const router = useRouter();
+
+  const textInputStyle =
+    "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-violet-500 focus:border-violet-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-violet-500 dark:focus:border-violet-500";
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
-    // TODO: get settings data
-    // TODO: get robots data
-    // TODO: get site & page data
-    // TODO: execute data
-    //execute(data);
+    let data = Object.fromEntries(
+      Array.from(formData.entries()).filter(([_, value]) => value != "")
+    ) as Partial<GenerateMetatagsInput>;
+    execute(data as GenerateMetatagsInput);
+    if (status === "hasSucceeded" && result.data != null) {
+      setOutput(result.data as string);
+      router.push("/generate/success");
+    }
   };
-
   return (
     <>
-      GOT OUTPUT: {JSON.stringify(result, null, 4)}
-      <div className="flex flex-col md:flex-row items-center justify-center min-h-screen">
-        <div className="w-full md:w-1/2 p-6">
-          <form
-            className="w-full max-w-lg mx-auto mt-10 bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-            onSubmit={handleSubmit}
+      <div className="mt-4 min-h-screen">
+        <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
+          {/* GENERAL SETTINGS */}
+          <div className="flex flex-col gap-3">
+            <h1 className="text-2xl font-bold underline underline-offset-8">
+              General settings ⚙️
+            </h1>
+            <p>
+              These settings will be used to generate the meta tags. enable or
+              disable as per your needs.
+            </p>
+            <div className="flex flex-col">
+              {settingsCheckboxes.map((checkbox, index) => (
+                <div key={index} className="mb-4 flex flex-row gap-1">
+                  <input
+                    type="checkbox"
+                    name={checkbox.name}
+                    id={checkbox.id}
+                    about={checkbox.about}
+                    className="mr-2"
+                    defaultChecked
+                  />
+                  <label
+                    htmlFor={checkbox.id}
+                    className="font-medium text-gray-700 dark:text-gray-300"
+                  >
+                    {checkbox.label}
+                  </label>
+                </div>
+              ))}
+            </div>
+          </div>
+          <hr className="my-2 border-[#E5E7EB] dark:border-[#90909084]" />
+          {/* ROBOTS SETTINGS */}
+          <div className="flex flex-col gap-3">
+            <h1 className="text-2xl font-bold underline underline-offset-8">
+              Robot settings 🤖
+            </h1>
+            <p>Control bots behaviour on your site.</p>
+            <div className="flex flex-col">
+              {robotCheckboxes.slice(0, 3).map((checkbox, index) => (
+                <div key={index} className="mb-4 flex flex-row gap-1">
+                  <input
+                    type="checkbox"
+                    name={checkbox.name}
+                    id={checkbox.id}
+                    about={checkbox.about}
+                    className="mr-2"
+                  />
+                  <label
+                    htmlFor={checkbox.id}
+                    className="font-medium text-gray-700 dark:text-gray-300"
+                  >
+                    {checkbox.label}
+                  </label>
+                </div>
+              ))}
+            </div>
+          </div>
+          <hr className="my-2 border-[#E5E7EB] dark:border-[#90909084]" />
+          {/* PROJECT SETTINGS */}
+          <div className="flex flex-col gap-3">
+            <h1 className="text-2xl font-bold underline underline-offset-8">
+              Project settings ⚙️
+            </h1>
+            <p>
+              These settings will be used to generate meta tags that represent
+              your project data.
+            </p>
+            <div className="flex flex-col">
+              {projectMetaData.map((input, index) => (
+                <div key={index} className="mb-4 flex flex-col gap-2">
+                  {input.type != "color" ? (
+                    input.required ? (
+                      <>
+                        <label htmlFor={input.name}>*{input.label}</label>
+                        <input
+                          key={input.key}
+                          type={input.type}
+                          name={input.name}
+                          id={input.id}
+                          placeholder={input.placeholder}
+                          className={textInputStyle}
+                          required
+                        />
+                      </>
+                    ) : (
+                      <>
+                        <label htmlFor={input.name}>{input.label}</label>
+                        <input
+                          key={input.key}
+                          type={input.type}
+                          name={input.name}
+                          id={input.id}
+                          placeholder={input.placeholder}
+                          className={textInputStyle}
+                        />
+                      </>
+                    )
+                  ) : (
+                    <div className="flex flex-row">
+                      <input
+                        key={input.key}
+                        type={input.type}
+                        name={input.name}
+                        id={input.id}
+                        placeholder={input.placeholder}
+                        className="mr-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-violet-500 focus:border-violet-500 block w-1/12 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-violet-500 dark:focus:border-violet-500"
+                      />
+                      <label
+                        htmlFor={input.id}
+                        key={input.key}
+                        className="font-medium text-gray-700 dark:text-gray-300"
+                      >
+                        {input.label}
+                      </label>
+                    </div>
+                  )}
+                </div>
+              ))}
+            </div>
+          </div>
+          <hr className="my-2 border-[#E5E7EB] dark:border-[#90909084]" />
+          {/* PAGE SETTINGS */}
+          <div className="flex flex-col gap-3">
+            <h1 className="text-2xl font-bold underline underline-offset-8">
+              Page settings ⚙️
+            </h1>
+            <p>
+              These settings will be used to generate meta tags that represent
+              your page data.
+            </p>
+            <div className="flex flex-col">
+              {pageMetaData.map((input, index) => (
+                <div key={index} className="mb-4 flex flex-col gap-1">
+                  {input.required ? (
+                    <>
+                      <label htmlFor={input.name}>*{input.label}</label>
+                      <input
+                        key={input.key}
+                        type={input.type}
+                        name={input.name}
+                        id={input.id}
+                        placeholder={input.placeholder}
+                        className={textInputStyle}
+                        required
+                      />
+                    </>
+                  ) : (
+                    <>
+                      <label htmlFor={input.name}>{input.label}</label>
+                      <input
+                        key={input.key}
+                        type={input.type}
+                        name={input.name}
+                        id={input.id}
+                        placeholder={input.placeholder}
+                        className={textInputStyle}
+                      />
+                    </>
+                  )}
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <button
+            type="submit"
+            value="Generate"
+            className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
           >
-            <input type="text" name="title" placeholder="Title" />
-            <input type="text" name="description" placeholder="Description" />
-            <input type="text" name="url" placeholder="URL" />
-            <input type="text" name="author" placeholder="Author" />
-            {/* <input type="text" name="img" placeholder="Image URL" />
-          <input type="text" name="img_alt" placeholder="Image Alt Text" /> */}
-            {/* <input
-            type="text"
-            name="twitter_card_type"
-            placeholder="Twitter Card Type"
-          />
-          <input
-            type="text"
-            name="twitter_handle"
-            placeholder="Twitter Handle"
-          />
-          <input type="text" name="name" placeholder="Name" />
-          <input type="text" name="generator" placeholder="Generator" />
-          <input type="checkbox" name="comments" />
-          <input
-            type="text"
-            name="css"
-            placeholder="CSS Files (comma separated)"
-          />
-          <input
-            type="text"
-            name="js"
-            placeholder="JS Files (comma separated)"
-          />
-          <textarea name="inline_css" placeholder="Inline CSS" />
-          <textarea name="inline_js" placeholder="Inline JS" /> */}
-            <button type="submit" value="Generate">
-              Generate
-            </button>
-          </form>
-        </div>
+            Generate
+          </button>
+        </form>
       </div>
     </>
   );
