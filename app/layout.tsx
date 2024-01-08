4945578f2c925d4f78334dd3e3efcadc7eade392
Prudhvi Suraaj <86603322+GPSxtreme@users.noreply.github.com>
2024-01-08 14:18:01 +0530

    Update layout description to include "Analyze &" and update nav border colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import ThemeSwitch from "./ui/components/ThemeSwitch";
 import Providers from "./providers";
 export const metadata: Metadata = {
   title: { default: "Meta editor", template: "%s | Meta editor" },
-  description: "Generate metadata for your website within seconds 🚀",
+  description: "Analyze & generate metadata for your website within seconds 🚀",
 };
 
 export default function RootLayout({
@@ -18,7 +18,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={`${openSans.className}`}>
         <Providers>
-          <nav className="flex flex-row justify-between m-3 p-3 border-b-2 border-dashed border-black dark:border-white">
+          <nav className="flex flex-row justify-between m-3 p-3 border-b-2 border-[#E5E7EB] dark:border-[#90909084]">
             <Link
               href={"/"}
               className={`${pacifico.className} text-xl md:text-4xl font-normal logo-hp`}
