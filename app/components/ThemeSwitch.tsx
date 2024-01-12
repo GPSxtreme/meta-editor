461a6101e5fb723d6e4e45af306f2d4b98e93fa0
Prudhvi Suraaj <86603322+GPSxtreme@users.noreply.github.com>
2024-01-13 01:44:58 +0530

    Add ThemeSwitch and UploadFileComponent components

---
+++ app/components/ThemeSwitch.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
+import { SunIcon, MoonIcon } from "@heroicons/react/24/outline";
+
+export default function ThemeSwitch() {
+	const { theme = "light", setTheme } = useTheme();
+	const [currentTheme, setCurrentTheme] = useState(theme);
+
+	useEffect(() => {
+		if (theme === "system") {
+			const darkModeMediaQuery = window.matchMedia(
+				"(prefers-color-scheme: light)",
+			);
+			setCurrentTheme(darkModeMediaQuery.matches ? "dark" : "light");
+		} else {
+			setCurrentTheme(theme);
+		}
+	}, [theme]);
+
+	const toggleTheme = () => {
+		if (currentTheme === "system") {
+			setTheme("light");
+		} else {
+			setTheme(currentTheme === "dark" ? "light" : "dark");
+		}
+	};
+
+	return (
+		<button
+			onClick={toggleTheme}
+			type="button"
+			className="relative inline-flex items-center space-x-2 transition duration-500 ease-in-out hover:scale-110"
+		>
+			{currentTheme === "dark" ? (
+				<MoonIcon className="w-6 h-6 transition duration-500 ease-in-out" />
+			) : (
+				<SunIcon className="w-6 h-6 transition duration-500 ease-in-out" />
+			)}
+		</button>
+	);
+}
