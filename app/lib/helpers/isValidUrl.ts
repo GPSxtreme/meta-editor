6dc5070a47fd56a618bb0b644a67c9ca3454d076
Prudhvi Suraaj <86603322+GPSxtreme@users.noreply.github.com>
2024-01-13 01:47:47 +0530

    Add isValidUrl helper function for URL validation

---
+++ app/lib/helpers/isValidUrl.ts
@@ -0,0 +1,5 @@
+export const isValidUrl =  (url: string) => {
+    return !(url.match(
+      /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g,
+    ) === null)
+}
